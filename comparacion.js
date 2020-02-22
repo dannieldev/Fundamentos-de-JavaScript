b1632b46478ba67b31ac50891b1f5576e652e8c7
Danielcresp <dneli6cres@outlook.com>
2020-02-22 17:12:36 -0600

    Comparacion en js

---
+++ comparacion.js
@@ -0,0 +1,17 @@
+var y = 4
+var x = 4
+var dobleigual = x == y //
+var tripeigual = x===y
+
+var sat ={
+    nombre: 'sath'
+}
+var sate ={
+    nombre: 'sath'
+}
+
+comp = sat===sate
+console.log(comp)
+var sat = sate // En la misma memoria
+
+
