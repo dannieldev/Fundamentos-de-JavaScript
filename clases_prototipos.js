56ac1d53347422c7226a9f98a169f59778cebd3b
Danielcresp <dneli6cres@outlook.com>
2020-02-24 21:53:25 -0600

    Clases prototipo con ES6

--- clases_prototipos.js
+++ clases_prototipos.js
@@ -1,41 +1,29 @@
 //Prototipos 
 //No hay clases hay prototipos 
-// No hay herencia de clase hay herencia prototipal
-function heredaDe(prototiposHijo, prototipoPadre){
-    var fn  = function(){}
-    fn.prototype = prototipoPadre.prototype
-    prototiposHijo.prototype = new fn
-    prototiposHijo.prototype.constructor = prototiposHijo  
-}
-function Persona(){
-    console.log('Good persona')
-}
-//Pasar parametro 
-function People(nombre,apellido,estatura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.estatura = estatura
-}
-People.prototype.saludar = function(){
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
-}
-People.prototype.alto = function(){
-    if(this.estatura >= 1.80){
-        console.log('Soy alto')
+// No hay herencia de clase hay herencia prototypal
+
+class People{
+    constructor (nombre,apellido,estatura){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.estatura = estatura
+    }
+    saludar(){
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+    }
+    alto(){
+        return  this.altura > 1.8
     }
 }
-function Desarrollador(nombre,apellido){
-    this.nombre = nombre 
-    this.apellido = apellido
+class Desarrollador extends People{
+    constructor (nombre,apellido,altura){
+        //Super es el constructor del padre
+        super(nombre,apellido,altura)
+    }
+    saludar(){
+        console.log(`Hola , me llamo ${this.nombre} ${this.apellido} y soy Programador`)
+    }
 }
-
-heredaDe(Desarrollador,People)
-
-Desarrollador.prototype.saludar = function (){
-    console.log(`Hola , me llamo ${this.nombre} ${this.apellido} y soy Programador`)
-}
-
-var daniel = new Persona()
 var dann = new People('Dann','cres',1.70)
 var frec = new People('Frec','Flezx',1.80)
 dann.saludar()
