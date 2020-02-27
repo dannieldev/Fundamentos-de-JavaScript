cb9afbed5991c6aff12ae9d3bde126c2b125af41
Danielcresp <dneli6cres@outlook.com>
2020-02-26 21:34:21 -0600

    Uso de CallBack para obtner request ordenada

--- callback.js
+++ callback.js
@@ -1,11 +1,26 @@
 const API_URL ='https://swapi.co/api/'
 const PEOPLE_URL='people/:id'
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts ={ crossDomain:true }
 
-const onPeopleResponse = function(persona){
-    console.log(`Hola yo soy ${persona.name}`)
+function obtenerPersonaje(id,callback){
+    const url =`${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $.get(url,opts,function(persona){
+        console.log(`Hola yo soy ${persona.name}`)
+        if(callback){
+            callback()
+        }
+    })
 }
+//Callback
+obtenerPersonaje(1,function(){
+    obtenerPersonaje(2, function(){
+        obtenerPersonaje(3,function(){
+            obtenerPersonaje(4,function(){
+                obtenerPersonaje(6, function(){
+                    obtenerPersonaje(7)
+                })
+            })
+        })
+    })
+})
 
-$.get(lukeUrl,opts,onPeopleResponse)
