5dcee99e9faa4b821dc31de35d8fdb71de743b85
Danielcresp <dneli6cres@outlook.com>
2020-02-26 22:20:36 -0600

    CallBack con mensaje de error

--- callback.js
+++ callback.js
@@ -4,23 +4,32 @@ const opts ={ crossDomain:true }
 
 function obtenerPersonaje(id,callback){
     const url =`${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    $.get(url,opts,function(persona){
-        console.log(`Hola yo soy ${persona.name}`)
-        if(callback){
-            callback()
-        }
+    
+    $.get(url,opts,callback).fail(function(){
+        console.log(`Sucedió un error. No se puede obtner el personaje ${id}`)
     })
 }
 //Callback
-obtenerPersonaje(1,function(){
-    obtenerPersonaje(2, function(){
-        obtenerPersonaje(3,function(){
-            obtenerPersonaje(4,function(){
-                obtenerPersonaje(6, function(){
-                    obtenerPersonaje(7)
+obtenerPersonaje(1,function(persona){
+    console.log(`Hola yo soy ${persona.name}`)
+
+    obtenerPersonaje(2, function(persona){
+        console.log(`Hola yo soy ${persona.name}`)
+
+        obtenerPersonaje(3,function(persona){
+            console.log(`Hola yo soy ${persona.name}`)
+
+            obtenerPersonaje(4,function(persona){
+                console.log(`Hola yo soy ${persona.name}`)
+
+                obtenerPersonaje(6, function(persona){
+                    console.log(`Hola yo soy ${persona.name}`)
+                    
+                    obtenerPersonaje(7, function(persona){
+                        console.log(`Hola yo soy ${persona.name}`)
+                    })
                 })
             })
         })
     })
 })
-
