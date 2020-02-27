144201c4288a72e1ef6d9f4f878f0fb59af0a96d
Danielcresp <dneli6cres@outlook.com>
2020-02-26 23:05:08 -0600

    Uso de Promesas para hacer peticiones a la API

--- callback.js
+++ callback.js
@@ -2,34 +2,23 @@ const API_URL ='https://swapi.co/api/'
 const PEOPLE_URL='people/:id'
 const opts ={ crossDomain:true }
 
-function obtenerPersonaje(id,callback){
+//PROMESA
+function obtenerPersonaje(id){
+    return new Promise((resolve, reject) =>{
     const url =`${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    
-    $.get(url,opts,callback).fail(() => {
-        console.log(`Sucedió un error. No se puede obtner el personaje ${id}`)
+    $
+    .get(url,opts,function(data){
+        resolve(data)
+    })
+    .fail(() => reject(id))
     })
 }
-//Callback
-obtenerPersonaje(1,function(persona){
-    console.log(`Hola yo soy ${persona.name}`)
+function onError(id){
+    console.log(`Sucedio un error al al obtner el personaje ${id}`)
+}
 
-    obtenerPersonaje(2, function(persona){
-        console.log(`Hola yo soy ${persona.name}`)
-
-        obtenerPersonaje(3,function(persona){
-            console.log(`Hola yo soy ${persona.name}`)
-
-            obtenerPersonaje(4,function(persona){
-                console.log(`Hola yo soy ${persona.name}`)
-
-                obtenerPersonaje(6, function(persona){
-                    console.log(`Hola yo soy ${persona.name}`)
-                    
-                    obtenerPersonaje(7, function(persona){
-                        console.log(`Hola yo soy ${persona.name}`)
-                    })
-                })
-            })
-        })
+obtenerPersonaje(1)
+    .then(function(personaje){
+        console.log(`EL personaje 1 es ${personaje.name}`)
     })
-})
+    .catch(onError)
