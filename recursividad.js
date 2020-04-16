1e9d05793e15e50bd4c10190c078cb5c427fdcfe
dannieldev <danielcrespin.dev@gmail.com>
2020-04-15 21:02:17 -0600

    Recursuvidad en javascript

---
+++ recursividad.js
@@ -0,0 +1,36 @@
+//CASO BASE
+/* 13 /__4__
+    13-4= 9     1
+    9-4=5       1
+    5-4=1       1
+    1-4=-3
+    
+ */
+
+
+ //Funcion RECURSIVA
+function divisionEntera(dividendo, divisor){
+    if(dividendo < divisor){
+        return 0
+    }
+    //CASO RECURSIVO
+
+    //LLama recursivamente a la misma funcion 
+    return 1 + divisionEntera(dividendo - divisor, divisor) //
+}
+
+//console.log(divisionEntera(13,4))
+//console.log(divisionEntera(3,2))
+
+//Dias faltantes para Domingo
+/* hoy =1 lunes 7= domingo
+
+*/ 
+function diasFaltantes (dia, otroDia){
+    if(dia === otroDia){
+        return 0
+    }
+    return 1 + diasFaltantes(dia+1, otroDia)
+}
+
+console.log(diasFaltantes(1,7))
